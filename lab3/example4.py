623dc0fb99d5bdf6044fbef842a9817b17c68047
b72446916 <erendemirel@ogr365.iyte.edu.tr>
2020-10-26 09:15:47 +0000

    .

--- lab3/example4.py
+++ lab3/example4.py
@@ -2,11 +2,12 @@ age = float(input("Please enter your age :"))
 normal_bus_ticket = 3
 discount = 0.5
 discount_price = normal_bus_ticket * discount
- if age < 6  :
-   print("You don't have to pay.")
- if age > 60 :
-   print("You don't have to pay.") 
- if age < 18 :
+if 6 <= age <= 60   :
+  if 6 <= age <= 18 :
    print(discount_price)
- else : 
-   print(normal_bus_ticket)
+  else:
+     print(normal_bus_ticket)
+  else:
+     print("You don't have to pay.")
+
+   
