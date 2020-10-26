0f9af9a8404858429ccbe380bf18abb0a45a40ab
b72446916 <erendemirel@ogr365.iyte.edu.tr>
2020-10-26 08:47:59 +0000

    .

--- lab3/example2.py
+++ lab3/example2.py
@@ -2,11 +2,4 @@ number1 = float(input("Please enter a number: "))
 number2 = float(input("Please enter a number: "))
 number3 = float(input("Please enter a number: "))
 
-if number1 < number2 < number3 :
-  print(number1)
-elif number2 < number1 < number3 :
-  print(number2)
-elif number3 < number2 < number1 :
-  print(number3)
-elif number3 < number1 < number2 :
-  print(number3)
+print("Minimum = " , min(number1,number2,number3))
