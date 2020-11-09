c7d27d985621064e5440755687b5ffccf7108ed8
b72446916 <erendemirel@ogr365.iyte.edu.tr>
2020-11-09 09:23:05 +0000

    .

---
+++ lab4/example3.py
@@ -0,0 +1,7 @@
+nums = [6,10,150,3,67,44]
+if len(nums) == 1 :
+  print(nums)
+else :
+  for i in range (len(nums)) :
+    
+
---
+++ lab4/example4.py
@@ -0,0 +1,7 @@
+a = int(input("Please enter a number. "))
+b = int(input("Please enter a number. "))
+while b < 0:
+  False
+  
+c = pow(a , b)
+print(c)
