3cc27907f880b111ca3abcf31c61421e61124ffd
b72446916 <erendemirel@ogr365.iyte.edu.tr>
2020-11-09 08:46:15 +0000

    .

---
+++ lab4/example1.py
@@ -0,0 +1,5 @@
+a = int(input("Bir adet sayı giriniz."))
+onlar_basamagı = a // 10
+print("sayının onlar basamagı = ", onlar_basamagı)
+birler_basamagı = a % 10
+print("sayının birler basamağı = ", birler_basamagı)
