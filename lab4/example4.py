81f6116adbd26e00166b473cd9a173eb5060cc9a
b72446916 <erendemirel@ogr365.iyte.edu.tr>
2020-11-09 09:31:16 +0000

    .

--- lab4/example4.py
+++ lab4/example4.py
@@ -2,6 +2,5 @@ a = int(input("Please enter a number. "))
 b = int(input("Please enter a number. "))
 while b < 0:
   False
-  
 c = pow(a , b)
 print(c)
---
+++ lab4/example5.py
@@ -0,0 +1,5 @@
+n = int(input("please entere a number."))
+x = 1 
+for i in range(n):
+  x *= i + 1
+  print(x)
