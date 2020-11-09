de9e8e5e821822e5a00496e0215586d532dbc122
b72446916 <erendemirel@ogr365.iyte.edu.tr>
2020-11-09 08:48:45 +0000

    .

--- lab4/example1.py
+++ lab4/example1.py
@@ -1,5 +1,8 @@
+#iki basamaklı sayılar için
 a = int(input("Bir adet sayı giriniz."))
 onlar_basamagı = a // 10
 print("sayının onlar basamagı = ", onlar_basamagı)
 birler_basamagı = a % 10
 print("sayının birler basamağı = ", birler_basamagı)
+c = birler_basamagı + onlar_basamagı
+print("Son iki basamağın toplamı = ", c)
