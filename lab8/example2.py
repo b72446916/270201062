ac4b4306d39ffe1079800fbe9edc7dd1eec0d271
b72446916 <erendemirel@ogr365.iyte.edu.tr>
2020-12-14 08:49:58 +0000

    .

---
+++ lab8/example2.py
@@ -0,0 +1,15 @@
+def is_prime(i):
+  i = int(input("type a number: \n"))
+  if i % 2 == 0:
+    print(i,"is a prime number")
+def print_primes_between(j,k):
+  j = int(input("type a number:\n"))
+  k = int(input("type a number:\n"))
+  if j > k :
+    print("invalid value.type a smaller value.")
+  for i in range(j,k):
+    while i > 2 :
+      if i % 2 != 0  :
+        print(i,"is a prime number.")
+        break
+print(print_primes_between("j","k"))      
