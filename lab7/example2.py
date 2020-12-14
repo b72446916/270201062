e7ddcea37c1fed83a0dbaf818e129e530c8059f1
b72446916 <erendemirel@ogr365.iyte.edu.tr>
2020-12-14 08:34:05 +0000

    .

---
+++ lab7/example2.py
@@ -0,0 +1,8 @@
+books = ["ULYSSES","ANIMAL FARM","BRAVE NEW WORLD","ENDER'S GAME"]
+book_dictionary= {}
+book_tuple = ()
+book_list =[book_tuple]
+for i in books:
+  book_list.append(len(i))
+
+print(book_list)
---
+++ lab8/example1.py
@@ -0,0 +1,8 @@
+a_list = [12, -7, 5, -89.4, 3, 27, 56, 57.3]
+def sum(a_list):
+  a = 0
+  for i in a_list:
+    a += i
+    return a
+
+print(sum(a_list))
